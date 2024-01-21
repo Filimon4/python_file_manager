90844757e9005f299038ee778a49975a5cd50bf1
Filimon4 <f.rykov@bk.ru>
2024-01-21 15:57:05 +0300

    basic file manager init

--- startWindow.py
+++ startWindow.py
@@ -1,37 +1,57 @@
-# This Python file uses the following encoding: utf-8
-from PySide6 import QtCore
-from PySide6.QtWidgets import QMainWindow, QMessageBox, QApplication
-from ui_mainwindow import Ui_MainWindow
+from PySide6.QtWidgets import QMainWindow, QFileSystemModel, QSplitter, QTreeView, QWidget, QVBoxLayout
+from PySide6.QtCore import QDir
 
-class StartWindow(QMainWindow,Ui_MainWindow):
-    def __init__(self, app):
+class StartWindow(QMainWindow):
+    def __init__(self):
         super().__init__()
-        self.setupUi(self)
-        self.app = app
 
-        self.actionQuit.triggered.connect(self.quit)
-        self.actionCopy.triggered.connect(self.copy)
-        self.actionCut.triggered.connect(self.cut)
-        self.actionPaste.triggered.connect(self.paste)
-        self.actionUndo.triggered.connect(self.undo)
-        self.actionRedo.triggered.connect(self.redo)
-        self.actionAbout.triggered.connect(self.about)
-        # self.actionAbout_Qt.triggered.connect(self.aboutqt)
+        self.setWindowTitle("File title")
+        self.setGeometry(100, 100, 800, 600)
 
-    def quit(self):
-        self.app.quit()
-    def copy(self):
-        self.textEdit.copy()
-    def cut(self):
-        self.textEdit.cut()
-    def paste(self):
-        self.textEdit.paste()
-    def undo(self):
-        self.textEdit.undo()
-    def redo(self):
-        self.textEdit.redo()
-    def about(self):
-        QMessageBox.information(self,"Going pro!","QMainWindow,Qt Designer and Resources : Going pro!")
-    def aboutqt(self):
-        QApplication.aboutQt()
+        self.central_widget = QWidget(self)
+        self.setCentralWidget(self.central_widget)
+
+        self.layout = QVBoxLayout(self.central_widget)
+
+        self.dialog = QFileSystemModel()
+        self.dialog.setRootPath(QDir.currentPath())
+
+        self.tree = QTreeView(self.central_widget)
+        self.tree.setModel(self.dialog)
+        self.tree.setRootIndex(self.dialog.index(QDir.rootPath()))
+
+
+        self.layout.addWidget(self.tree)
+
+        # class SimpleFileManager(QMainWindow):
+        #     def __init__(self):
+        #         super(SimpleFileManager, self).__init__()
+
+        #         self.setWindowTitle("Simple File Manager")
+        #         self.setGeometry(100, 100, 800, 600)
+
+        #         self.central_widget = QWidget(self)
+        #         self.setCentralWidget(self.central_widget)
+
+        #         self.layout = QVBoxLayout(self.central_widget)
+
+        #         # File Tree
+        #         self.model = QFileSystemModel()
+        #         self.model.setRootPath(QDir.rootPath())  # Display files starting from the root directory
+
+        #         self.tree_view = QTreeView(self.central_widget)
+        #         self.tree_view.setModel(self.model)
+        #         self.tree_view.setRootIndex(self.model.index(QDir.rootPath()))
+
+        #         self.layout.addWidget(self.tree_view)
+
+
+
+
+
+
+
+
+
+
 
