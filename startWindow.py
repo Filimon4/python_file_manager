fd36a520726c9d871f1d7a978cc8ca24d9e186f5
Filimon4 <f.rykov@bk.ru>
2024-01-22 23:57:44 +0300

    make proggress

--- startWindow.py
+++ startWindow.py
@@ -1,7 +1,8 @@
-from PySide6.QtWidgets import QMainWindow, QFileSystemModel, QSplitter, QTreeView, QWidget, QVBoxLayout
+from PySide6.QtWidgets import QMainWindow, QFileSystemModel, QTreeView, QWidget, QVBoxLayout, QLabel, QTreeWidget, QSplitter
 from PySide6.QtCore import QDir
+from ui_mainwindow import Ui_MainWindow
 
-class StartWindow(QMainWindow):
+class StartWindow(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
 
@@ -13,37 +14,30 @@ class StartWindow(QMainWindow):
 
         self.layout = QVBoxLayout(self.central_widget)
 
-        self.dialog = QFileSystemModel()
+        self.filePath = QLabel()
+        self.filePath.setText(f"Current: {QDir.root().dirName()}")
+
+        self.dialog = QFileSystemModel(self)
         self.dialog.setRootPath(QDir.currentPath())
+        self.dialog.rootPathChanged.connect(self.pathChanged)
 
         self.tree = QTreeView(self.central_widget)
         self.tree.setModel(self.dialog)
-        self.tree.setRootIndex(self.dialog.index(QDir.rootPath()))
+        self.tree.doubleClicked.connect(self.treeClicked)
+        self.tree.setRootIndex(self.dialog.index(QDir.root().dirName()))
 
-
+        self.layout.addWidget(self.filePath)
         self.layout.addWidget(self.tree)
 
-        # class SimpleFileManager(QMainWindow):
-        #     def __init__(self):
-        #         super(SimpleFileManager, self).__init__()
 
-        #         self.setWindowTitle("Simple File Manager")
-        #         self.setGeometry(100, 100, 800, 600)
 
-        #         self.central_widget = QWidget(self)
-        #         self.setCentralWidget(self.central_widget)
+    def pathChanged(self, path):
+        print(path)
 
-        #         self.layout = QVBoxLayout(self.central_widget)
-
-        #         # File Tree
-        #         self.model = QFileSystemModel()
-        #         self.model.setRootPath(QDir.rootPath())  # Display files starting from the root directory
-
-        #         self.tree_view = QTreeView(self.central_widget)
-        #         self.tree_view.setModel(self.model)
-        #         self.tree_view.setRootIndex(self.model.index(QDir.rootPath()))
-
-        #         self.layout.addWidget(self.tree_view)
+    def treeClicked(self, index):
+        file = self.dialog.filePath(index)
+        self.filePath.setText(f"Current: {file}")
+        self.tree.setRootIndex(self.dialog.index(file))
 
 
 
